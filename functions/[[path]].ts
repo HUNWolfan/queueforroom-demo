8882636aae32e9f10fb72158457ec4cca5c82a0d
HUNWolfan <hunwolfan12@gmail.com>
2025-11-26 17:40:28 +0100

    🗑️ Remove /functions directory - incompatible with Cloudflare Pages

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-
-// @ts-ignore - This file is generated during the build
-import * as build from "../build/server";
-
-export const onRequest = createPagesFunctionHandler({
-  build,
-  getLoadContext: (context) => ({ env: context.env }),
-});
